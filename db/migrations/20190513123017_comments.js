201f346f556db0186c9a964ffdb973a009732b77
Adrian Percival <ahpercival@gmail.com>
2019-05-13 12:38:17 +0100

    migration completed

---
+++ db/migrations/20190513123017_comments.js
@@ -0,0 +1,19 @@
+
+exports.up = function (knex, Promise) {
+    console.log('creating comments table...');
+    return knex.schema.createTable('comments', commentsTable => {
+        commentsTable.increments('comment_id').primary()
+        commentsTable.string('author').notNullable();
+        commentsTable.foreign('author').references('users.username')
+        commentsTable.integer('article_id').notNullable();
+        commentsTable.foreign('article_id').references('articles.article_id')
+        commentsTable.integer('votes').defaultTo(0).notNullable();
+        commentsTable.timestamp('created_at', { useTz: true })
+        commentsTable.text('body').notNullable();
+    })
+};
+
+exports.down = function (knex, Promise) {
+    console.log('removing comments tables...');
+    return knex.schema.dropTable('comments');
+};
