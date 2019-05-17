768b90ee31d919cd453021919220554f26c97c50
Adrian Percival <ahpercival@gmail.com>
2019-05-17 12:58:44 +0100

    heroku changes based on hosting.md

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,10 @@
-const dbConfig = process.env.PORT ? process.env.PORT : require('../knexfile');
+const ENV = process.env.NODE_ENV || 'development';
+const knex = require('knex');
 
-module.exports = require('knex')(dbConfig);
+const dbConfig =
+    ENV === 'production'
+        ? { client: 'pg', connection: process.env.DATABASE_URL }
+        : require('../knexfile');
+
+module.exports = knex(dbConfig);
+// module.exports = require('knex')(dbConfig) <<<PREVIOUSLY?
