c09946b1f9bf641f3371722bdc2050084a375f76
Adrian Percival <ahpercival@gmail.com>
2019-06-09 13:15:53 +0100

    user data updated

--- db/data/dev-data/users.js
+++ db/data/dev-data/users.js
@@ -2,12 +2,12 @@ module.exports = [
   {
     username: 'tickle122',
     name: 'Tom Tickle',
-    avatar_url: 'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg',
+    avatar_url: 'https://i1.wp.com/metro.co.uk/wp-content/uploads/2014/03/wpid-article-1312912102057-0d5dc73100000578-807569_636x482.jpg?quality=90&strip=all&zoom=1&resize=540%2C409',
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg',
+    avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest/scale-to-width-down/250?cb=20170707233013',
   },
   {
     username: 'happyamy2016',
@@ -17,16 +17,16 @@ module.exports = [
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
-    avatar_url: 'https://i.imgur.com/WfX0Neu.jpg',
+    avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/c/c2/Mr-cool-1a.jpg/revision/latest/scale-to-width-down/250?cb=20170519070512',
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
-    avatar_url: 'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg',
+    avatar_url: 'https://res.cloudinary.com/dxgjsxlzb/image/fetch/w_600,c_limit,q_auto/https://www.hunted.com/blog/wp-content/uploads/2018/04/Mr-Bump1.jpg',
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
-    avatar_url: 'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg',
+    avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141',
   },
 ];
